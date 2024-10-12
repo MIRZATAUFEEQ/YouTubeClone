f77fb1f85939159c97d30384e9ddd8b5e2da4d03
Mirza <mirzataufeeq98436@gmail.com>
2024-10-12 13:35:51 +0530

    video9 custome api ApiResponse and error handling

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cookeiParser from 'cookie-parser'
+import cors from 'cors'
+const app = express()
+
+
+app.use(cors({      //middleware
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({ limit: '16kb' }))//configration
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))//configration
+app.use(express.static("public"))//configration
+app.use(cookeiParser())//configration
+export { app }
---
+++ src/index.js
@@ -0,0 +1,22 @@
+// const dotenv = require('dotenv')
+import dotenv from 'dotenv'
+import connectDB from './db/db.js'
+import { app } from './app.js'
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running at port http://localhost:${process.env.PORT}`)
+            app.on('error', (error) => {
+                console.log('error:', error)
+                throw error
+            })
+        })
+    })
+    .catch((err) => {
+        console.log('mongodb connection failed', err)
+    })
