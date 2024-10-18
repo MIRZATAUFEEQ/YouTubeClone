a3e7887a83589d88a523dc7557be5a658355c60f
Mirza <mirzataufeeq98436@gmail.com>
2024-10-18 18:01:33 +0530

    video13

--- src/app.js
+++ src/app.js
@@ -13,4 +13,10 @@ app.use(express.json({ limit: '16kb' }))//configration
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))//configration
 app.use(express.static("public"))//configration
 app.use(cookeiParser())//configration
+
+//routers
+import userRouter from './routes/user.routes.js'
+app.use('/api/v1/users', userRouter)
+
+
 export { app }
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,20 +1,21 @@
-const asyncHandler = (func) => async (req, res, next) => {
-    try {
-        await func(req, res, next)
+// const asyncHandler = (func) => async (req, res, next) => {
+//     try {
+//         await func(req, res, next)
 
-    } catch (error) {
-        res.status(error.code || 500).json({
-            success: false,
-            message: error.message
-        })
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
+// export { asyncHandler }
+
+
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error))
     }
 }
 
 export { asyncHandler }
-
-
-// const asyncHandler=(requestHandler)=>{
-//     (req,res,next)=>{
-//         Promise.resolve(requestHandler(req,res,next)).catch((error)=>next(error))
-//     }
-// }
