d67c7fb49a3e7825afcfbde35460a582ff0c7f09
Mirza <mirzataufeeq98436@gmail.com>
2024-12-01 15:59:35 +0530

    index files

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ dotenv.config({
 connectDB()
     .then(() => {
         app.listen(process.env.PORT || 8000, () => {
-            console.log(`server is running at port http://localhost:${process.env.PORT}`)
+            console.log(`⚙️ server is running at port http://localhost:${process.env.PORT}`)
             app.on('error', (error) => {
                 console.log('error:', error)
                 throw error
