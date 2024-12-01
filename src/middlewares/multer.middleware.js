6b8d8d65c63b4331864431fdca2ac09ae6f3fc2b
Mirza <mirzataufeeq98436@gmail.com>
2024-12-01 16:01:25 +0530

    multer middleware

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,10 +1,18 @@
 import multer from 'multer'
+import path from 'path'
+import fs from 'fs'
 
+
+const uploadDir = './public/temp'
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true })
+    console.log(`created directory at ${uploadDir}`)
+}
 const storage = multer.diskStorage({
-    destinaiton: function (req, res, cb) {
-        cb(null, '/public/temp')
+    destinaiton: function (req, file, cb) {
+        cb(null, uploadDir)
     },
-    filename: function (req, res, cb) {
+    filename: function (req, file, cb) {
         // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
         cb(null, file.originalname)
 
