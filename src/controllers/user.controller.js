f23707d2840adfea5c3750889a292219cada9861
Mirza <mirzataufeeq98436@gmail.com>
2026-01-22 18:46:26 +0530

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -417,9 +417,4 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-<<<<<<< HEAD
-    getUserChannelProfile,
-    getWatchHistory
-=======
->>>>>>> 57bf26d9f305364ba8230cc991e51f96d42806ac
 }
